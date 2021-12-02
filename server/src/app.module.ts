488eeb9328e22e6a66c6efed09c846f2293edb22
Axel Malawski <axelmalawski@gmail.com>
2021-12-02 13:50:37 -0300

    finished

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-import { Module } from '@nestjs/common';
+import { HostParam, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
@@ -12,19 +12,19 @@ import { FoldersModule } from './folders/folders.module';
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
+const { HOST, PORT, USERNAME, PASSWORD, DATABASE } = process.env;
+
 @Module({
   controllers: [AppController],
   providers: [AppService],
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'ec2-184-73-25-2.compute-1.amazonaws.com',
-      port: 5432,
-      username: 'igprdpdkqaxlcd',
-      password:
-        'c33265d8dade206160d83e5338b99a5e4827c6c50bb088d9e69e871ef907d4a1',
-
-      database: 'd22gfo68s8cio',
+      host: HOST,
+      port: parseInt(PORT),
+      username: USERNAME,
+      password: PASSWORD,
+      database: DATABASE,
       entities: [Task, Folder],
       synchronize: true,
       ssl: true,
